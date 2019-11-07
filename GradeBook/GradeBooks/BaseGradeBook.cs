e91cf3bc73ecc0b550b6c199445cb1830bf3be95
randallfine <randallfine@gmail.com>
2019-11-06 21:40:14 -0600

    Completed module

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -15,7 +15,7 @@ namespace GradeBook.GradeBooks
         
         public List<Student> Students { get; set; }
 
-        public GradeBookType Type { get; set; };
+        public GradeBookType Type { get; set; }
 
         public BaseGradeBook(string name)
         {

512f7f87f609b62259c0ea94e80844c47114922a
randallfine <randallfine@gmail.com>
2019-11-06 21:43:03 -0600

    Completed module

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook:BaseGradeBook
+    public class StandardGradeBook:BaseGradeBook
     {
         public StandardGradeBook(string name):base(name)
         {
